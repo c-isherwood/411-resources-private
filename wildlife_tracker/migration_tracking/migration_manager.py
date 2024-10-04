f83a11661566f595a91a3444555b6162a538ae28
Campbell Isherwood <campbellisherwood@Campbells-MacBook-Pro.local>
2024-10-04 09:43:53 -0400

    requirements branch all my work for hw3

---
+++ wildlife_tracker/migration_tracking/migration_manager.py
@@ -0,0 +1,42 @@
+from typing import Optional
+from wildlife_tracker.migration_tracking.migration import Migration
+from wildlife_tracker.migration_tracking.migration_path import MigrationPath
+from wildlife_tracker.habitat_management.habitat import Habitat
+
+
+class MigrationManager:
+
+    pass
+
+def cancel_migration(migration_id: int) -> None:
+    pass
+
+def get_migration_by_id(migration_id: int) -> Migration:
+    pass
+
+def get_migration_details(migration_id: int) -> dict[str, Any]:
+    pass
+
+def get_migrations() -> list[Migration]:
+    pass
+
+def get_migrations_by_current_location(current_location: str) -> list[Migration]:
+    pass
+
+def get_migrations_by_migration_path(migration_path_id: int) -> list[Migration]:
+    pass
+
+def get_migrations_by_start_date(start_date: str) -> list[Migration]:
+    pass
+
+def get_migrations_by_status(status: str) -> list[Migration]:
+    pass
+
+def get_migration_path_details(path_id) -> dict:
+    pass
+
+def schedule_migration(migration_path: MigrationPath) -> None:
+    pass
+
+def update_migration_details(migration_id: int, **kwargs: Any) -> None:
+    pass
